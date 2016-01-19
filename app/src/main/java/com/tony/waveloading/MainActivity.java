1145794c1859479a57a53e6324c8bc4dac05f531
sanyinchen <1554194922@qq.com>
2016-01-20 01:07:13 +0800

    developing

--- app/src/main/java/com/tony/waveloading/MainActivity.java
+++ app/src/main/java/com/tony/waveloading/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tony.waveloading;
 
+import com.tony.wavelibrary.WaveLoadingView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        WaveLoadingView waveLoadingView = new WaveLoadingView(this);
+        setContentView(waveLoadingView);
     }
 }
--- wavelibrary/src/main/java/com/tony/wavelibrary/WaveLoadingView.java
+++ wavelibrary/src/main/java/com/tony/wavelibrary/WaveLoadingView.java
@@ -3,8 +3,14 @@
  */
 package com.tony.wavelibrary;
 
+import android.animation.AnimatorSet;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,16 +18,51 @@ import android.view.View;
  * Created by sanyinchen on 16/1/19.
  */
 public class WaveLoadingView extends View implements WaveLoadingInterface {
+    // draw
+    private Context mContext;
+
+    // Dynamic Properties.
+    private int mCanvasSize;
+    private float mAmplitudeRatio;
+    private int mWaveColor;
+    private int mShapeType;
+
+    // Properties.
+    private String mTopTitle;
+    private String mCenterTitle;
+    private String mBottomTitle;
+    private float mDefaultWaterLevel;
+
+    // Object used to draw.
+    // Shader containing repeated waves.
+    private BitmapShader mWaveShader;
+    private Bitmap bitmapBuffer;
+    // Shader matrix.
+    private Matrix mShaderMatrix;
+    // Paint to draw wave.
+    private Paint mWavePaint;
+    // Paint to draw border.
+    private Paint mBorderPaint;
+
+    // Animation.
+    private AnimatorSet mAnimatorSet;
+
+    // Default config
+    private static final float DEFAULT_AMPLITUDE_RATIO = 0.1f;
+    private static final float DEFAULT_WATER_LEVEL_RATIO = 0.5f;
+    private static final float DEFAULT_WAVE_LENGTH_RATIO = 1.0f;
+    private static final float DEFAULT_WAVE_SHIFT_RATIO = 0.0f;
+    private static final int DEFAULT_WAVE_PROGRESS_VALUE = 50;
+    private static final int DEFAULT_WAVE_COLOR = Color.parseColor("#212121");
+    private static final int DEFAULT_TITLE_COLOR = Color.parseColor("#212121");
+    private static final float DEFAULT_BORDER_WIDTH = 0;
+    private static final float DEFAULT_TITLE_TOP_SIZE = 18.0f;
+    private static final float DEFAULT_TITLE_CENTER_SIZE = 22.0f;
+    private static final float DEFAULT_TITLE_BOTTOM_SIZE = 18.0f;
+
     public WaveLoadingView(Context context) {
         super(context);
-    }
-
-    public WaveLoadingView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public WaveLoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        init(context);
     }
 
     @Override
@@ -29,8 +70,27 @@ public class WaveLoadingView extends View implements WaveLoadingInterface {
 
     }
 
+    private void init(Context mContext) {
+        this.mContext = mContext;
+
+        // Init Wave.
+        mShaderMatrix = new Matrix();
+        mWavePaint = new Paint();
+        // The ANTI_ALIAS_FLAG bit AntiAliasing smooths out the edges of what is being drawn,
+        // but is has no impact on the interior of the shape.
+        mWavePaint.setAntiAlias(true);
+
+        mWaveColor = DEFAULT_WAVE_COLOR;
+
+
+        mWavePaint = new Paint();
+        mWavePaint.setAntiAlias(true);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvas.drawCircle(100, 100, 50, mWavePaint);
     }
 }
